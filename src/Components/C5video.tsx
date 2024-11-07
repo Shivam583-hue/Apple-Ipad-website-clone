aa312136bd6a14f3e92e50d525c7833889f8f506
Shivam <shivamshivamshivam456@gmail.com>
2024-11-07 06:05:45 +0530

    Update C5video.tsx

--- src/Components/C5video.tsx
+++ src/Components/C5video.tsx
@@ -1,18 +1,45 @@
-//need to add the tag of thickness scrollTrigger: {trigger: '.c5',scrub: 1,start: 'bottom bottom'}
-import gsap from "gsap"
-import ScrollTrigger from 'gsap/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
-import { useEffect } from "react"
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const C5video = () => {
+  const videoRef = useRef(null);
+
   useEffect(() => {
-    gsap.fromTo(".c5", {opacity: 0 ,y:0}, {opacity: 1,y:-1400, duration: 8, ease: "power2.inOut", })
-  }, [])
+    const handleVideoPlay = () => {
+      gsap.fromTo(
+        ".c5",
+        { opacity: 0, y: 0 },
+        {
+          opacity: 1,
+          y: -1400,
+          duration: 8,
+          ease: "power2.inOut",
+          scrollTrigger: {
+            trigger: ".c5",
+            scrub: 1,
+            start: "bottom bottom",
+          },
+        }
+      );
+    };
+
+    const videoElement = videoRef.current;
+    videoElement.addEventListener("play", handleVideoPlay);
+
+    return () => {
+      videoElement.removeEventListener("play", handleVideoPlay);
+    };
+  }, []);
+
   return (
     <div className="bg-black"> 
       <div className="bg-black flex justify-center mx-auto pt-[50px]">
         <video 
-          className="w-[50%] h-auto  rounded-lg shadow-lg"
+          ref={videoRef}
+          className="w-[50%] h-auto rounded-lg shadow-lg"
           src="/video.webm"
           autoPlay
           muted 
@@ -21,12 +48,12 @@ const C5video = () => {
           <source src="/video.webm" type="video/webm" />
         </video>
       </div>
-      <div className="flex c5 flex-col pl-[55%] ">
-          <h1 className="text-white text-[70px] font-sans font-semibold leading-tight">5.1 mm</h1>
-          <h1 className="text-[#767677] text-[20px] font-sans font-semibold">thin and light design</h1>
+      <div className="flex c5 flex-col pl-[55%]">
+        <h1 className="text-white text-[70px] font-sans font-semibold leading-tight">5.1 mm</h1>
+        <h1 className="text-[#767677] text-[20px] font-sans font-semibold">thin and light design</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default C5video
+export default C5video;
